c53b258c2e8c6062d6b007ba689a1300afe09b28
Rumman Khan <classicrumman@gmail.com>
2026-05-15 23:55:44 +0530

    update vite confug

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/react-vite-cicd/',
+
   test: {
     globals: true,
-    base: '/react-vite-cicd/',
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+
     coverage: {
       reporter: ['text', 'html'],
     },
